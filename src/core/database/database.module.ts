61789ac65341b72ab00bb5df2b93d1dbe156296e
Kareem Arafa <kareem.arafa@afaqy.com>
2021-09-26 04:50:32 +0200

    fix mongo connection

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -3,6 +3,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://kareem_1:cohqom-jifgE4-pufpym@cluster0.2pd2c.mongodb.net/resume?retryWrites=true&w=majority')]
+  imports: [MongooseModule.forRoot('mongodb-srv://kareem_1:cohqom-jifgE4-pufpym@cluster0.2pd2c.mongodb.net/resume?retryWrites=true&w=majority')]
 })
 export class DatabaseModule {}
